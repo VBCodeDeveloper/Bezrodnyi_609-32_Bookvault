d395f52ed8762ef244936c42d68e48bdf8195696
VB <>
2025-05-29 21:49:05 +0500

    .idea ignore

--- bookvault/settings.py
+++ bookvault/settings.py
@@ -38,8 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'corsheaders',
+
     'store',
     'accounts',
+    'books',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +53,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+    "http://localhost:8000",
 ]
 
 ROOT_URLCONF = 'bookvault.urls'
@@ -119,6 +129,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- bookvault/urls.py
+++ bookvault/urls.py
@@ -14,6 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -22,4 +25,5 @@ urlpatterns = [
 
     path('', include('store.urls')),
     path('accounts/', include('accounts.urls')),
-]
+    path('api/', include('books.urls'))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
